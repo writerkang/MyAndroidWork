6cc02a1876f16b098c40dc4f3c64fa3e17716a93
writerkang <goodwriterkang@gmail.com>
2020-04-15 00:36:36 +0900

    Calculator 1st version

--- a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
@@ -3,13 +3,136 @@ package com.lec.android.a003_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    Button btnC, btnEqul, btnPlus, btnMinus, btnMulti, btnDiv;
+    EditText textResult;
+    String cal1;
+    String cal2;
+    String symbol;
 
     //과졔: 계산기 앱 만들기
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textResult = findViewById(R.id.textResult);
+
+        btn0 = findViewById(R.id.button0);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+        btn7 = findViewById(R.id.button7);
+        btn8 = findViewById(R.id.button8);
+        btn9 = findViewById(R.id.button9);
+
+        btnEqul = findViewById(R.id.buttonEqual);
+        btnEqul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal2 = textResult.getText().toString();
+
+                if(symbol.equals("+")) {
+                    int a = Integer.parseInt(cal1) + Integer.parseInt(cal2);
+
+                    textResult.setText(String.valueOf(a));
+                } else if(symbol.equals("-")) {
+                    int a = Integer.parseInt(cal1) - Integer.parseInt(cal2);
+
+                    textResult.setText(String.valueOf(a));
+                } else if(symbol.equals("X")) {
+                    int a = Integer.parseInt(cal1) * Integer.parseInt(cal2);
+
+                    textResult.setText(String.valueOf(a));
+                } else {
+                    int a = Integer.parseInt(cal1) / Integer.parseInt(cal2);
+
+                    textResult.setText(String.valueOf(a));
+                }
+
+            }
+        });
+
+        btnPlus = findViewById(R.id.buttonPlus);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal1 = textResult.getText().toString();
+                symbol = "+";
+                textResult.setText("");
+                Log.d("myapp", cal1);
+            }
+        });
+
+        btnMinus = findViewById(R.id.buttonMinus);
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal1 = textResult.getText().toString();
+                symbol = "-";
+                textResult.setText("");
+                Log.d("myapp", cal1);
+            }
+        });
+
+        btnMulti = findViewById(R.id.buttonMulti);
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal1 = textResult.getText().toString();
+                symbol = "X";
+                textResult.setText("");
+                Log.d("myapp", cal1);
+            }
+        });
+
+        btnDiv = findViewById(R.id.buttonDiv);
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal1 = textResult.getText().toString();
+                symbol = "÷";
+                textResult.setText("");
+                Log.d("myapp", cal1);
+            }
+        });
+
+        btnC = findViewById(R.id.buttonC);
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textResult.setText("");
+            }
+        });
+
+        class numberListener implements View.OnClickListener{
+
+
+            @Override
+            public void onClick(View v) {
+                String text = (String)((Button)v).getText();
+                textResult.setText(textResult.getText().append(text));
+            }
+        }
+
+        btn0.setOnClickListener(new numberListener());
+        btn1.setOnClickListener(new numberListener());
+        btn2.setOnClickListener(new numberListener());
+        btn3.setOnClickListener(new numberListener());
+        btn4.setOnClickListener(new numberListener());
+        btn5.setOnClickListener(new numberListener());
+        btn6.setOnClickListener(new numberListener());
+        btn7.setOnClickListener(new numberListener());
+        btn8.setOnClickListener(new numberListener());
+        btn9.setOnClickListener(new numberListener());
     }
 }
